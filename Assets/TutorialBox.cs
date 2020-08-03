97f0838dfd0370ad733f90c5d9d16379ecff2123
Dan Miller <dsmiller95@gmail.com>
2020-08-03 15:06:58 -0500

    added a bunch of tutorial messages to the earlier levels, along with example actions in the list

---
+++ Assets/TutorialBox.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TutorialBox : MonoBehaviour
+{
+    public TutorialBox nextBox;
+
+    public void AdvanceToNextBox()
+    {
+        gameObject.SetActive(false);
+        nextBox?.gameObject.SetActive(true);
+    }
+
+    public void SkipTutorial()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
